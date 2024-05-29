1c285c83c8fe6a4f27379d2de53e724b736131bb
karvendhanm <karvsmech@gmail.com>
2024-05-29 09:10:14 +0530

    Dataframe selector custom transformer

--- house_prices_prediction/util.py
+++ house_prices_prediction/util.py
@@ -7,10 +7,22 @@ class DropMissingData(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
-    def fit(self, X: pd.DataFrame):
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X:pd.DataFrame):
-        return X.dropna()
+        return X.dropna().values
 
-    
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+
+
